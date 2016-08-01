c3eb9b4da4e353dd3249398fde9fc980fc90a72e
Alexis Chandler <alexiscchandler@gmail.com>
2016-07-31 21:48:42 -0400

    initial commit

---
+++ Virtual/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            VirtualPet pokemon2 = new VirtualPet();
+            pokemon2.Queen();
+            string hellobunny;
+            pokemon2.Print();
+            Console.WriteLine();
+            string interact;
+
+            do
+            {
+                Console.WriteLine();
+                pokemon2.Interact();
+                Console.WriteLine();
+                interact = Console.ReadLine().ToUpper();
+                Console.Clear();
+                pokemon2.Queen();
+
+
+
+                if (interact == "FEED")
+                {
+                    Console.WriteLine("To feed me, pick one of these: \nCarrot, Candy, Cherries, Veggies, or Soda");
+                    interact = Console.ReadLine().ToUpper();
+
+                    switch (interact)
+                    {
+
+
+                        case "CARROT":
+                            pokemon2.Eat();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Health Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+
+                            break;
+                        case "CANDY":
+                            pokemon2.Eatr();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Health Decreased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+                            break;
+                        case "CHERRIES":
+                            pokemon2.Eat();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Health Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+                            break;
+                        case "VEGGIES":
+                            pokemon2.Eat();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Health Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+                            break;
+                        case "SODA":
+                            pokemon2.Eatr();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Health Decreased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+                            break;
+
+                    }
+                    Console.WriteLine();
+                }
+
+                if (interact == "PLAY")
+                {
+                    Console.WriteLine("To play with me, pick one of these numbers: \n1- Throw the Striped Ball \n2- Throw the Bear Trap \n3- Throw the Chew Toy");
+                    interact = Console.ReadLine().ToUpper();
+
+                    switch (interact)
+                    {
+
+
+                        case "1":
+                            pokemon2.Playfulness();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Playfulness Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+
+                            break;
+                        case "2":
+                            pokemon2.Playfulnessr();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Playfulness Decreased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+                            break;
+                        case "3":
+                            pokemon2.Playfulness();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine("   My Playfulness Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+                            break;
+
+                    }
+                    Console.WriteLine();
+                }
+
+                if (interact == "EXERCISE")
+                {
+                    Console.WriteLine("To exercise me, pick one of these numbers: \n1- Sit Around the Garden \n2- Run Around the Garden \n3- Walk Around the Garden");
+                    interact = Console.ReadLine().ToUpper();
+
+                    switch (interact)
+                    {
+
+
+                        case "1":
+                            pokemon2.Workr();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine(" My Exercise Health Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+
+                            break;
+                        case "2":
+                            pokemon2.Work();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine(" My Exercise Health Decreased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                            pokemon2.Print();
+
+                            break;
+                        case "3":
+                            pokemon2.Work();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            Console.WriteLine();
+                            Console.WriteLine(" My Exercise Health Increased !");
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            pokemon2.Print();
+
+                            break;
+                    }
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    pokemon2.Interact();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to interact with Fluffy Bunny again?");
+                hellobunny = Console.ReadLine().ToUpper();
+                pokemon2.Tick();
+                Console.Clear();
+                pokemon2.Queen();
+                pokemon2.Print();
+
+
+
+            }
+            while (hellobunny == "YES");
+
+            Console.Clear();
+
+            interact = Console.ReadLine().ToUpper();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Virtual/VirtualPet.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual
+{
+    class VirtualPet
+    {
+
+        //fields
+        private int feed;
+        private int happy;
+        private int play;
+        private int exercise;
+
+
+        //properties
+
+        public int Hunger
+        {
+            get { return this.feed; }
+            set { this.Hunger = value; }
+        }
+
+        public int Happiness
+        {
+            get { return this.happy; }
+            set { this.Happiness = value; }
+        }
+
+        public int Playful
+        {
+            get { return this.play; }
+            set { this.Playful = value; }
+        }
+
+        public int WorkOut
+        {
+            get { return this.exercise; }
+            set { this.WorkOut = value; }
+        }
+
+
+
+        //constructors
+
+        public VirtualPet()
+        {
+            this.feed = 50;
+            this.happy = 50;
+            this.play = 50;
+            this.exercise = 50;
+        }
+
+
+        public void Eat()
+        {
+            feed = feed + 10;
+        }
+
+        public void Eatr()
+        {
+            feed = feed - 15;
+        }
+
+        public void Playfulness()
+        {
+            play = play + 10;
+        }
+
+        public void Playfulnessr()
+        {
+            play = play - 14;
+        }
+
+        public void Work()
+        {
+            exercise = exercise + 10;
+        }
+
+        public void Workr()
+        {
+            exercise = exercise - 12;
+        }
+
+        public void Tick()
+        {
+            feed = feed - 2;
+            happy = happy + 1;
+            play = play - 2;
+            exercise = exercise - 3;
+
+        }
+
+
+        public void Print()
+        {
+            StringBuilder princess = new StringBuilder();
+            princess.Append("\nFluffy Bunny's Health: " + this.feed);
+            princess.Append("\nFluffy bunny Happiness: " + this.happy);
+            princess.Append("\nfluffy bunny's Playfulness: " + this.play);
+            princess.Append("\nFluffy Bunny's Exercise Health: " + this.exercise);
+            Console.WriteLine(princess.ToString());
+        }
+
+        public void Interact()
+        {
+            Console.WriteLine("Rules: \nType \"Feed\", \"Play\", or \"Exercise\" to interact with me. \nMy Happiness increases the more you interact with me!");
+        }
+
+
+        public void Queen()
+        {
+            Console.WriteLine("hello friend! my name is fluffy bunny! to keep me happy, healthy, and sane you \nmust feed me the right food, play with me, and take me for walks! ");
+            Console.WriteLine();
+            Console.WriteLine(" /)_/)");
+            Console.WriteLine();
+            Console.WriteLine(" ( . .)");
+            Console.WriteLine();
+            Console.WriteLine("C('')('')");
+
+        }
+
+        }
+}
